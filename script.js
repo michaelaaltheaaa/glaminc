598e801a5bd2f06598defc51f77110a4475f9930
michaelaaltheaaa <163153071+michaelaaltheaaa@users.noreply.github.com>
2024-03-17 18:13:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         aboutSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
 
         // Adjust hero image size for better visibility on mobile devices
-        var heroImage = document.getElementById("hero-image");
+        var heroImage = document.getElementById("home__pic-container");
         home.style.maxWidth = "100%"; // Set maximum width to 100% to ensure it fits within the viewport width
         home.style.height = "auto"; // Allow the height to adjust proportionally based on the width
     }
